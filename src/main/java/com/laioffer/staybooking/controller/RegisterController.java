09323d0768ccbb2074bc044ded195057d2936bc1
Chenaiier <tangchen718@gmail.com>
2023-02-22 13:31:42 -0600

    staybooking

---
+++ src/main/java/com/laioffer/staybooking/controller/RegisterController.java
@@ -0,0 +1,30 @@
+package com.laioffer.staybooking.controller;
+
+import com.laioffer.staybooking.model.UserRole;
+import com.laioffer.staybooking.service.RegisterService;
+import com.laioffer.staybooking.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+    private RegisterService registerService;
+
+    @Autowired
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @PostMapping("/register/guest")
+    public void addGuest(@RequestBody User user) {
+        registerService.add(user, UserRole.ROLE_GUEST);
+    }
+
+    @PostMapping("/register/host")
+    public void addHost(@RequestBody User user) {
+        registerService.add(user, UserRole.ROLE_HOST);
+    }
+
+}
---
+++ src/main/java/com/laioffer/staybooking/service/StayService.java
@@ -0,0 +1,77 @@
+package com.laioffer.staybooking.service;
+
+import com.laioffer.staybooking.exception.StayDeleteException;
+import com.laioffer.staybooking.exception.StayNotExistException;
+import com.laioffer.staybooking.model.*;
+import com.laioffer.staybooking.repository.LocationRepository;
+import com.laioffer.staybooking.repository.ReservationRepository;
+import com.laioffer.staybooking.repository.StayReservationDateRepository;
+import org.springframework.stereotype.Service;
+import com.laioffer.staybooking.repository.StayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class StayService {
+    private StayRepository stayRepository;
+    private LocationRepository locationRepository;
+    private ImageStorageService imageStorageService;
+    private GeoCodingService geoCodingService;
+    private ReservationRepository reservationRepository;
+    private StayReservationDateRepository stayReservationDateRepository;
+
+    @Autowired
+    public StayService(StayRepository stayRepository, LocationRepository locationRepository, ReservationRepository reservationRepository, ImageStorageService imageStorageService, GeoCodingService geoCodingService, StayReservationDateRepository stayReservationDateRepository) {
+        this.stayRepository = stayRepository;
+        this.locationRepository = locationRepository;
+        this.reservationRepository = reservationRepository;
+        this.imageStorageService = imageStorageService;
+        this.geoCodingService = geoCodingService;
+        this.stayReservationDateRepository = stayReservationDateRepository;
+    }
+    public List<Stay> listByUser(String username) {
+        return stayRepository.findByHost(new User.Builder().setUsername(username).build());
+    }
+
+    public Stay findByIdAndHost(Long stayId, String username) throws StayNotExistException {
+        Stay stay = stayRepository.findByIdAndHost(stayId, new User.Builder().setUsername(username).build());
+        if (stay == null) {
+            throw new StayNotExistException("Stay doesn't exist");
+        }
+        return stay;
+    }
+
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void add(Stay stay, MultipartFile[] images) {
+        List<String> mediaLinks = Arrays.stream(images).parallel().map(image -> imageStorageService.save(image)).collect(Collectors.toList());
+        List<StayImage> stayImages = new ArrayList<>();
+        for (String mediaLink : mediaLinks) {
+            stayImages.add(new StayImage(mediaLink, stay));
+        }
+        stay.setImages(stayImages);
+        stayRepository.save(stay);
+
+        Location location = geoCodingService.getLatLng(stay.getId(), stay.getAddress());
+        locationRepository.save(location);
+    }
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    public void delete(Long stayId, String username) throws StayNotExistException, StayDeleteException {
+        Stay stay = stayRepository.findByIdAndHost(stayId, new User.Builder().setUsername(username).build());
+        if (stay == null) {
+            throw new StayNotExistException("Stay doesn't exist");
+        }
+        List<Reservation> reservations = reservationRepository.findByStayAndCheckoutDateAfter(stay, LocalDate.now());
+        if (reservations != null && reservations.size() > 0) {
+            throw new StayDeleteException("Cannot delete stay with active reservation");
+        }
+        stayRepository.deleteById(stayId);
+    }
+}
